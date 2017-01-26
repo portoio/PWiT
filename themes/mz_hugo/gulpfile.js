9e2de5175e4c7146b20fc5178d46ab4420df7294
Luis Correia <luis@marzeelabs.org>
2017-01-26 11:27:09 +0000

    Add first version of SASS compiler

---
+++ themes/mz_hugo/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    nano = require('gulp-cssnano'),
+    runSequence = require('run-sequence');
+
+
+/**
+ * Tasks
+ */
+
+gulp.task('sass', function() {
+  return gulp.src("scss/*.scss") // Gets all files ending with .scss
+    .pipe(sass())
+    .pipe(concat('app.css'))
+    .pipe(nano())
+    .pipe(gulp.dest('static/css'));
+});
+
+
+gulp.task('watch', function(){
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch']);
